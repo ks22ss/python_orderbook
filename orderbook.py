dd3c656171d02f04be7c37074b84ac4857bfd17e
kingson299 <kingson22.work@gmail.com>
2022-10-27 19:42:58 +0800

    create orderbook

---
+++ orderbook.py
@@ -0,0 +1,99 @@
+from ast import Or
+from collections import deque
+from dataclasses import dataclass
+from shutil import ExecError
+
+@dataclass
+class LimitOrderInfo:
+    id: str
+    price: float
+    volume: int
+    side: str
+
+@dataclass
+class MarketOrderInfo:
+    id: str
+    volume: int
+    side: str
+
+class Orderbook:
+    def __init__(self) -> None:
+        self.book = {}
+    
+    def add_new_price(self, price):
+        self.book[price] = deque([])
+    
+    def validate_side_input(self, order):
+        if order.side != "bid" and order.side != "ask":
+            raise Exception("[Error] order.side is either 'bid' and 'ask'.")  
+
+    def validate_volume_input(self, order):
+        if order.volume <= 0:
+            raise Exception("[Error] order.volume must be larger than 0.")  
+
+    def validate_price_input(self, order):
+        if order.side == 'bid':
+            if len(self.filter_book_by_side('ask')) > 0 and order.price >= min(self.filter_book_by_side('ask').keys()):
+                raise Exception("[Error] limit price exceed current ask.") 
+        elif order.side == 'ask': 
+            if len(self.filter_book_by_side('bid')) > 0 and order.price <= max(self.filter_book_by_side('bid').keys()):
+                raise Exception("[Error] limit price exceed current bid.") 
+
+    def place_limit_order(self, order: LimitOrderInfo):
+        if order.price not in self.book:
+            self.add_new_price(order.price)
+
+        self.validate_side_input(order)
+        self.validate_price_input(order)
+
+        self.book[order.price].appendleft({'id':order.id, 'volume':order.volume, 'side':order.side})
+
+    def remove_empty_order_lists(self, book):
+        return {k:v for k,v in book.items() if len(v) > 0}
+
+    def filter_book_by_side(self, side):
+        new_book = {}
+        for price_level, order_list in self.book.items():
+            new_book[price_level] = list(filter(lambda order_info: order_info['side'] == side, order_list))
+        return self.remove_empty_order_lists(new_book)
+
+    def sort_book_by_price(self, book, side):
+        reverse = False
+        if side == 'bid':
+            reverse = True
+        return sorted(book.items(), reverse=reverse)
+
+    def place_market_order(self, order: MarketOrderInfo):
+        self.validate_side_input(order)
+        self.validate_volume_input(order)
+
+        new_book = self.filter_book_by_side(order.side)
+        new_book = self.sort_book_by_price(new_book, order.side)
+
+        current_vol = order.volume
+        i = 0
+        while current_vol > 0:
+            
+            price_level = new_book[i][0]
+            order_lists = new_book[i][1]
+
+            for order_info in order_lists:
+                #print(order_info)
+                if current_vol >= order_info['volume']:
+                    self.book[price_level].popleft()
+                    current_vol -= order_info['volume']
+                else:
+                    self.book[price_level][0]['volume'] -= current_vol
+                    current_vol = 0
+            i += 1
+
+        self.book = self.remove_empty_order_lists(self.book)
+        
+
+
+if __name__ == '__main__':
+    orderbook = Orderbook()
+    orderbook.place_limit_order(LimitOrderInfo("A01",20, 100,'bid'))
+    orderbook.place_limit_order(LimitOrderInfo("A01",20, 100,'ask'))
+    orderbook.place_limit_order(LimitOrderInfo("A01",20, 100,'bid'))
+    print(orderbook.book)
---
+++ tests/test_orderbook.py
@@ -0,0 +1,103 @@
+import unittest
+from orderbook import Orderbook, LimitOrderInfo, MarketOrderInfo
+from collections import deque
+
+class TestOrderBook(unittest.TestCase):
+
+    def test_limit_order_placing_1(self):
+        orderbook = Orderbook()
+        orderbook.place_limit_order(LimitOrderInfo("A01",20, 100,'bid'))
+        with self.assertRaises(Exception) as context:
+            orderbook.place_limit_order(LimitOrderInfo("A01",19, 100,'ask'))
+            self.assertTrue('[Error] limit price exceed current bid.' in context.exception)
+
+    def test_limit_order_placing_2(self):
+        orderbook = Orderbook()
+        orderbook.place_limit_order(LimitOrderInfo("A01",20, 100,'bid'))
+        orderbook.place_limit_order(LimitOrderInfo("A01",22, 200,'ask'))
+        with self.assertRaises(Exception) as context:
+            orderbook.place_limit_order(LimitOrderInfo("A01",24, 300,'bid'))
+            self.assertTrue('[Error] limit price exceed current ask.' in context.exception)
+
+    def test_market_order_placing_3(self):
+        orderbook = Orderbook()
+        orderbook.place_limit_order(LimitOrderInfo("LO00001",25, 100,'bid'))
+        orderbook.place_limit_order(LimitOrderInfo("LO00002",25, 200,'bid'))
+        orderbook.place_limit_order(LimitOrderInfo("LO00003",24, 300,'bid'))
+        orderbook.place_limit_order(LimitOrderInfo("LO00004",23, 250,'bid'))
+        
+        orderbook.place_market_order(MarketOrderInfo("MO00001",50,'bid'))
+        self.assertEqual(orderbook.book, {
+                25: deque([{'id': 'LO00002', 'volume': 150, 'side': 'bid'}, 
+                            {'id': 'LO00001', 'volume': 100, 'side': 'bid'}]), 
+                24: deque([{'id': 'LO00003', 'volume': 300, 'side': 'bid'}]),
+                23: deque([{'id': 'LO00004', 'volume': 250, 'side': 'bid'}])
+            })
+
+    
+    def test_market_order_placing_4(self):
+        orderbook = Orderbook()
+        orderbook.place_limit_order(LimitOrderInfo("LO00001",25, 100,'ask'))
+        orderbook.place_limit_order(LimitOrderInfo("LO00002",25, 200,'ask'))
+        orderbook.place_limit_order(LimitOrderInfo("LO00003",24, 300,'ask'))
+        orderbook.place_limit_order(LimitOrderInfo("LO00004",23, 250,'ask'))
+        
+        orderbook.place_market_order(MarketOrderInfo("MO00001",50,'ask'))
+        self.assertEqual(orderbook.book, {
+            25: deque([{'id': 'LO00002', 'volume': 200, 'side': 'ask'}, 
+                        {'id': 'LO00001', 'volume': 100, 'side': 'ask'}]), 
+            24: deque([{'id': 'LO00003', 'volume': 300, 'side': 'ask'}]), 
+            23: deque([{'id': 'LO00004', 'volume': 200, 'side': 'ask'}])
+        })
+
+    def test_market_order_placing_5(self):
+        orderbook = Orderbook()
+        orderbook.place_limit_order(LimitOrderInfo("LO00001",25, 100,'ask'))
+        orderbook.place_limit_order(LimitOrderInfo("LO00002",25, 150,'ask'))
+        orderbook.place_limit_order(LimitOrderInfo("LO00003",24, 50,'ask'))
+        orderbook.place_limit_order(LimitOrderInfo("LO00004",23, 200,'ask'))
+        
+        orderbook.place_market_order(MarketOrderInfo("MO00001",500,'ask'))
+        self.assertEqual(orderbook.book, {})
+
+
+    def test_market_order_placing_6(self):
+        orderbook = Orderbook()
+        orderbook.place_limit_order(LimitOrderInfo("LO00001",25, 100,'bid'))
+        orderbook.place_limit_order(LimitOrderInfo("LO00002",25, 150,'bid'))
+        orderbook.place_limit_order(LimitOrderInfo("LO00003",24, 50,'bid'))
+        orderbook.place_limit_order(LimitOrderInfo("LO00004",23, 200,'bid'))
+        
+        orderbook.place_market_order(MarketOrderInfo("MO00001",500,'bid'))
+        self.assertEqual(orderbook.book, {})
+
+
+    def test_market_order_placing_7(self):
+        orderbook = Orderbook()
+        orderbook.place_limit_order(LimitOrderInfo("LO00001",27, 100,'ask'))
+        orderbook.place_limit_order(LimitOrderInfo("LO00002",25, 150,'ask'))
+        orderbook.place_limit_order(LimitOrderInfo("LO00003",24, 50,'ask'))
+        orderbook.place_limit_order(LimitOrderInfo("LO00004",23, 200,'ask'))
+        
+        orderbook.place_market_order(MarketOrderInfo("MO00001",450,'ask'))
+        self.assertEqual(orderbook.book, {27: deque([{'id': 'LO00001', 'volume': 50, 'side': 'ask'}])})
+
+    def test_market_order_placing_8(self):
+        orderbook = Orderbook()
+        orderbook.place_limit_order(LimitOrderInfo("LO00002",13, 150,'ask'))
+        orderbook.place_limit_order(LimitOrderInfo("LO00003",12, 50,'bid'))
+        with self.assertRaises(Exception) as context:
+            orderbook.place_market_order(MarketOrderInfo("MO00001",0,'bid'))
+            self.assertTrue("[Error] order.volume must be larger than 0." in context.exception)
+
+    def test_market_order_placing_9(self):
+        orderbook = Orderbook()
+        orderbook.place_limit_order(LimitOrderInfo("LO00002",13, 150,'ask'))
+        orderbook.place_limit_order(LimitOrderInfo("LO00003",12, 50,'bid'))
+        with self.assertRaises(Exception) as context:
+            orderbook.place_market_order(MarketOrderInfo("MO00001",0,'mark'))
+            self.assertTrue("[Error] order.side is either 'bid' and 'ask'." in context.exception)
+
+
+if __name__ == '__main__':
+    unittest.main()
